6d561305867a0474edcba397348a0d293776b8af
Ben <meowcat@meowcat.site>
2026-05-11 10:29:19 +0100

    i hate read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,8 @@
+project = 'JosuFramework'
+
+html_theme = 'sphinx_rtd_theme'
+
 html_theme_options = {
-    'project': 'JosuFramework'
-    'html_theme': 'sphinx_rtd_theme'
     'logo_only': False,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
@@ -9,10 +11,11 @@ html_theme_options = {
     'flyout_display': 'hidden',
     'version_selector': True,
     'language_selector': True,
+
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
-    'titles_only': False
+    'titles_only': False,
 }
