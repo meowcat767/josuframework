bc5eb157b19a41d76449b6bb80fa0f7019f32fc0
Ben <meowcat@meowcat.site>
2026-05-11 10:20:44 +0100

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,16 @@
-    html_theme_options = {
-      'logo_only': False,
-      'prev_next_buttons_location': 'bottom',
-      'style_external_links': False,
-      'vcs_pageview_mode': '',
-      'style_nav_header_background': 'white',
-      'flyout_display': 'hidden',
-      'version_selector': True,
-      'language_selector': True,
-      # Toc options
-      'collapse_navigation': True,
-      'sticky_navigation': True,
-      'navigation_depth': 4,
-      'includehidden': True,
-      'titles_only': False
-    }
+html_theme_options = {
+    'logo_only': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    'flyout_display': 'hidden',
+    'version_selector': True,
+    'language_selector': True,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
