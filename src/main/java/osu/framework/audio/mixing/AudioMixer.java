45b238f7bd0984f7f7b99f2d21d6abc0516a2088
Ben <meowcat@meowcat.site>
2026-05-11 16:47:35 +0100

    re-write mixers

--- src/main/java/osu/framework/audio/mixing/AudioMixer.java
+++ src/main/java/osu/framework/audio/mixing/AudioMixer.java
@@ -1,40 +1,65 @@
 package osu.framework.audio.mixing;
 
-import osu.framework.audio.AdjustableAudioComponent;
+import osu.framework.audio.AudioComponent;
+import osu.framework.audio.mixing.IAudioChannel;
+
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * An audio mixer that combines multiple audio channels.
- * TODO: Implement actual mixing logic with Java audio backend.
- */
-public class AudioMixer extends AdjustableAudioComponent implements IAudioMixer {
-    private final List<IAudioChannel> channels = new ArrayList<>();
+public abstract class AudioMixer
+        extends AudioComponent
+        implements IAudioMixer {
 
-    @Override
+    private final List<IAudioChannel> channels =
+            new ArrayList<>();
+
+    private final AudioMixer fallbackMixer;
+
+    protected AudioMixer(AudioMixer fallbackMixer) {
+        this.fallbackMixer = fallbackMixer;
+    }
+
     public void add(IAudioChannel channel) {
-        if (!channels.contains(channel)) {
-            channels.add(channel);
-            channel.bindAdjustments(this);
-        }
+
+        if (channel.getMixer() == this)
+            return;
+
+        if (channel.getMixer() != null)
+            channel.getMixer().remove(channel, false);
+
+        addInternal(channel);
+
+        channels.add(channel);
+
+        channel.setMixer(this);
     }
 
-    @Override
     public void remove(IAudioChannel channel) {
-        if (channels.remove(channel)) {
-            channel.unbindAdjustments(this);
-        }
+        remove(channel, true);
     }
 
-    @Override
-    protected void updateChildren() {
-        super.updateChildren();
+    protected void remove(
+            IAudioChannel channel,
+            boolean returnToDefault
+    ) {
+        if (!channels.contains(channel))
+            return;
 
-        // Update all channels
-        for (IAudioChannel channel : new ArrayList<>(channels)) {
-            if (channel instanceof AdjustableAudioComponent) {
-                ((AdjustableAudioComponent) channel).update();
-            }
-        }
+        removeInternal(channel);
+
+        channels.remove(channel);
+
+        channel.setMixer(null);
+
+        if (returnToDefault && fallbackMixer != null)
+            fallbackMixer.add(channel);
     }
+
+    protected abstract void addInternal(
+            IAudioChannel channel
+    );
+
+    protected abstract void removeInternal(
+            IAudioChannel channel
+    );
 }
--- src/main/java/osu/framework/audio/mixing/IAudioChannel.java
+++ src/main/java/osu/framework/audio/mixing/IAudioChannel.java
@@ -6,8 +6,24 @@ import osu.framework.audio.IAdjustableAudioComponent;
  * An interface for audio channels in a mixer.
  */
 public interface IAudioChannel extends IAdjustableAudioComponent {
+
     /**
      * Gets the mixer this channel belongs to.
      */
     IAudioMixer getMixer();
+
+    /**
+     * Sets the mixer this channel belongs to.
+     */
+    void setMixer(IAudioMixer mixer);
+
+    /**
+     * Reads audio samples into the provided buffer.
+     *
+     * @param buffer The destination buffer.
+     * @param offset The buffer offset.
+     * @param length The number of samples to read.
+     * @return The number of samples actually read.
+     */
+    int read(float[] buffer, int offset, int length);
 }
